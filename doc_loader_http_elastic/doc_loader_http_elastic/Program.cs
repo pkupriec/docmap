170bda78623f48e7beb66726753219f859401b3a
Petro kupriiec <p.kupriec@gmail.com>
2019-09-29 14:02:46 +0300

    initial commit

---
+++ doc_loader_http_elastic/doc_loader_http_elastic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace doc_loader_http_elastic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
